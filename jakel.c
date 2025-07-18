bd0005fb80c9a093fb7edd75952a626e349932a2
Tyson Horvath <48546805+tavro@users.noreply.github.com>
2025-07-18 19:43:46 +0200

    ✨ GH-JT: Implement file input
    Add functionality for reading file contents into memory. Also, fix bug related to moving the cursor.

--- jakel.c
+++ jakel.c
@@ -1,13 +1,21 @@
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 #define JAKEL_VERSION "0.0.1"
+#define JAKEL_TAB_STOP 8
 
 #define K_CTRL(k) ((k) & 0x1f)
 
@@ -26,11 +34,31 @@ enum keys {
 	PAGE_DOWN
 };
 
+typedef struct line {
+	int size;
+	int rsize;
+
+	char *chars;
+	char *render;
+} line;
+
 struct config {
 	int cx, cy;
+	int rx;
+
+	int rowOffset;
+	int colOffset;
 
 	int rows;
 	int cols;
+
+	int rowAmount;
+	line *row;
+
+	char *filename; // TODO: Update name
+
+	char statusMsg[80];
+	time_t statusMsgTime;
 
 	struct termios ot;
 };
@@ -181,6 +209,82 @@ int getSize(int *rs, int *cs) {
 	}
 }
 
+int rowCxToRx(line *row, int cx) {
+	int rx = 0;
+	int j;
+	for ( j = 0; j < cx; j++) {
+		if (row->chars[j] == '\t') {
+			rx += (JAKEL_TAB_STOP - 1) - (rx % JAKEL_TAB_STOP);
+		}
+		rx++;
+	}
+
+	return rx;
+}
+
+void updateRow(line *row) {
+	int tabs = 0;
+	int j;
+	for (j = 0; j < row->size; j++) {
+		if (row->chars[j] == '\t') tabs++;
+	}
+
+	free(row->render);
+	row->render = malloc(row->size + tabs*(JAKEL_TAB_STOP - 1) + 1);
+
+	int idx = 0;
+	for (j = 0; j < row->size; j++) {
+		if (row->chars[j] == '\t') {
+			row->render[idx++] = ' ';
+			while (idx % JAKEL_TAB_STOP != 0) row->render[idx++] = ' ';
+		} else {
+			row->render[idx++] = row->chars[j];
+		}
+	}
+
+	row->render[idx] = '\0';
+	row->rsize = idx;
+}
+
+void appendRow(char *s, size_t len) {
+	C.row = realloc(C.row, sizeof(line) * (C.rowAmount + 1));
+
+	int at = C.rowAmount;
+	C.row[at].size = len;
+	C.row[at].chars = malloc(len + 1);
+
+	memcpy(C.row[at].chars, s, len);
+
+	C.row[at].chars[len] = '\0';
+
+	C.row[at].rsize = 0;
+	C.row[at].render = NULL;
+	updateRow(&C.row[at]);
+
+	C.rowAmount++;
+}
+
+
+void open(char *filename) {
+	free(C.filename);
+	C.filename = strdup(filename);
+
+	FILE *fp = fopen(filename, "r");
+	if (!fp) kill("fopen");
+
+	char *l = NULL;
+	size_t lcap = 0;
+	ssize_t llen;
+
+	while((llen = getline(&l, &lcap, fp)) != -1) {
+		while (llen > 0 && (l[llen - 1] == '\n' || l[llen - 1] == '\r')) llen--;
+		appendRow(l, llen);
+	}
+
+	free(l);
+	fclose(fp);
+}
+
 struct abuf {
 	char *b;
 	int len;
@@ -201,7 +305,9 @@ void freeBuf(struct abuf *ab) {
 	free(ab->b);
 }
 
-void moveCursor(char c) {
+void moveCursor(int c) {
+	line *row = (C.cy >= C.rowAmount) ? NULL : &C.row[C.cy];
+
 	switch (c) {
 		case ARROW_UP:
 			if (C.cy != 0) {
@@ -211,18 +317,30 @@ void moveCursor(char c) {
 		case ARROW_LEFT:
 			if (C.cx != 0) {
 				C.cx--;
+			} else if (C.cy > 0) {
+				C.cy--; // IDEA: Stay on same line instead?
+				C.cx = C.row[C.cy].size;
 			}
 			break;
 		case ARROW_RIGHT:
-			if (C.cx != C.cols - 1) {
+			if (row && C.cx < row->size) {
 				C.cx++;
+			} else if (row && C.cx == row->size) {
+				C.cy++; // IDEA: Stay on same line instead?
+				C.cx = 0;
 			}
 			break;
 		case ARROW_DOWN:
-			if (C.cy != C.rows - 1) {
+			if (C.cy < C.rowAmount) {
 				C.cy++;
 			}
 			break;
+	}
+
+	row = (C.cy >= C.rowAmount) ? NULL : &C.row[C.cy];
+	int rowlen = row ? row->size : 0;
+	if (C.cx > rowlen) {
+		C.cx = rowlen;
 	}
 }
 
@@ -240,12 +358,19 @@ void processKey() {
 			C.cx = 0;
 			break;
 		case END:
-			C.cx = C.cols - 1;
+			if (C.cy < C.rowAmount) C.cx = C.row[C.cy].size;
 			break;
 
 		case PAGE_UP:
 		case PAGE_DOWN:
 			{
+				if (c == PAGE_UP) {
+					C.cy = C.rowOffset;
+				} else if (c == PAGE_DOWN) {
+					C.cy = C.rowOffset + C.rows - 1;
+					if (C.cy > C.rowAmount) C.cy = C.rowAmount;
+				}
+
 				int t = C.rows;
 				while (t--) moveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
 			}
@@ -260,44 +385,109 @@ void processKey() {
 	}
 }
 
-void drawRowIndicators(struct abuf *ab) {
-	int row;
-	for (row = 0; row < C.rows; row++) {
-		if (row == C.rows / 3) {
-			char headerMsg[80];
-			int headerMsgLen = snprintf(headerMsg, sizeof(headerMsg), "jakel -- version %s", JAKEL_VERSION);
+void scroll() {
+	C.rx = 0;
+	if (C.cy < C.rowAmount) {
+		C.rx = rowCxToRx(&C.row[C.cy], C.cx);
+	}
 
-			if (headerMsgLen > C.cols) headerMsgLen = C.cols;
+	if (C.cy < C.rowOffset) {
+		C.rowOffset = C.cy;
+	}
 
-			int padding = (C.cols - headerMsgLen) / 2;
-			if (padding) {
-				appendToBuf(ab, "-", 1);
-				padding--;
-			}
-			while (padding--) appendToBuf(ab, " ", 1);
+	if (C.cy >= C.rowOffset + C.rows) {
+		C.rowOffset = C.cy - C.rows + 1;
+	}
 
-			appendToBuf(ab, headerMsg, headerMsgLen);
-		} else {
-			appendToBuf(ab, "-", 1);
-		}
+	if (C.rx < C.colOffset) {
+		C.colOffset = C.rx;
+	}
 
-		appendToBuf(ab, "\x1b[K", 3);
-		if (row < C.rows - 1) {
-			appendToBuf(ab, "\r\n", 2);
-		}
+	if (C.rx >= C.colOffset + C.cols) {
+		C.colOffset = C.rx - C.cols + 1;
 	}
 }
 
+void draw(struct abuf *ab) {
+	int row;
+	for (row = 0; row < C.rows; row++) {
+		int fileRow = row + C.rowOffset;
+		if (fileRow >= C.rowAmount) {
+			if (C.rowAmount == 0 && row == C.rows / 3) {
+				char headerMsg[80];
+				int headerMsgLen = snprintf(headerMsg, sizeof(headerMsg), "jakel -- version %s", JAKEL_VERSION);
+
+				if (headerMsgLen > C.cols) headerMsgLen = C.cols;
+
+				int padding = (C.cols - headerMsgLen) / 2;
+				if (padding) {
+					appendToBuf(ab, "-", 1);
+					padding--;
+				}
+				while (padding--) appendToBuf(ab, " ", 1);
+
+				appendToBuf(ab, headerMsg, headerMsgLen);
+			} else {
+				appendToBuf(ab, "-", 1);
+			}
+		} else {
+			int len = C.row[fileRow].rsize - C.colOffset;
+			if (len < 0) len = 0;
+			if (len > C.cols) len = C.cols;
+			appendToBuf(ab, &C.row[fileRow].render[C.colOffset], len);
+		}
+
+		appendToBuf(ab, "\x1b[K", 3);
+		appendToBuf(ab, "\r\n", 2);
+	}
+}
+
+void drawStatusBar(struct abuf *ab) {
+	appendToBuf(ab, "\x1b[7m", 4);
+
+	char status[80], rstatus[80];
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines", C.filename ? C.filename : "[Unnamed]", C.rowAmount);
+	int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", C.cy + 1, C.rowAmount);
+
+	if (len > C.cols) len = C.cols;
+	appendToBuf(ab, status, len);
+
+	while (len < C.cols) {
+		if (C.cols - len == rlen) {
+			appendToBuf(ab, rstatus, rlen);
+			break;
+		} else {
+			appendToBuf(ab, " ", 1);
+			len++;
+		}
+	}
+
+	appendToBuf(ab, "\x1b[m", 3);
+	appendToBuf(ab, "\r\n", 2);
+}
+
+void drawMsgBar(struct abuf *ab) {
+	appendToBuf(ab, "\x1b[K", 3);
+
+	int msgLen = strlen(C.statusMsg);
+	if (msgLen > C.cols) msgLen = C.cols;
+	if (msgLen && time(NULL) - C.statusMsgTime < 5) appendToBuf(ab, C.statusMsg, msgLen);
+}
+
 void clear() {
+	scroll();
+
 	struct abuf ab = ABUF_INIT;
 
 	appendToBuf(&ab, "\x1b[?25l", 6);
 	appendToBuf(&ab, "\x1b[H", 3);
 
-	drawRowIndicators(&ab);
+	draw(&ab);
+	drawStatusBar(&ab);
+	drawMsgBar(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", C.cy + 1, C.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (C.cy - C.rowOffset) + 1, (C.rx - C.colOffset) + 1);
 	appendToBuf(&ab, buf, strlen(buf));
 
 	appendToBuf(&ab, "\x1b[?25h", 6);
@@ -306,16 +496,40 @@ void clear() {
 	freeBuf(&ab);
 }
 
+void setStatusMsg(const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(C.statusMsg, sizeof(C.statusMsg), fmt, ap);
+	va_end(ap);
+	C.statusMsgTime = time(NULL);
+}
+
 void init() {
 	C.cx = 0;
 	C.cy = 0;
+	C.rx = 0;
+	C.rowOffset = 0;
+	C.colOffset = 0;
+	C.rowAmount = 0;
+	C.row = NULL;
+	C.filename = NULL;
+	C.statusMsg[0] = '\0';
+	C.statusMsgTime = 0;
 
 	if (getSize(&C.rows, &C.cols) == -1) kill("getSize");
+
+	C.rows -= 2;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 	enterRawMode();
 	init();
+
+	if (argc >= 2) {
+		open(argv[1]);
+	}
+
+	setStatusMsg("CTRL-Q = quit");
 
 	while (1) {
 		clear();
